65b42c131b93e81c9be1c46dc1eb1f1d94f40284
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-03-24 15:17:12 -0300

    add origins, importações de base,engine e cors

--- main.py
+++ main.py
@@ -5,14 +5,33 @@ from routers.userRoutes import user_router
 from erros.errors import FinanceException
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from database.models import Base
+from database.db import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='Finance API',
               description='API de gestão de finanças pessoais',
               version='1.0')
 
+
+origins = [
+    'http://localhost',
+    'http://localhost:5500',
+    'http://127.0.0.1:8000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 app.include_router(transaction_router)
 app.include_router(account_router)
 app.include_router(user_router)
+
 
 @app.exception_handler(FinanceException)
 async def finance_exception_handler(request: Request, exc: FinanceException):
@@ -22,8 +41,8 @@ async def finance_exception_handler(request: Request, exc: FinanceException):
                             "message":exc.message_error,
                             "path":  request.url.path
                         })
-
 @app.get('/')
 async def request_home():
-    return 'Olá , api Finance está no Ar'
+    return{"Mensagem":'Olá , api Finance está no Ar'}
 
+Base.metadata.create_all(bind=engine)
