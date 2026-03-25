e16fffa99e3339ebbee387cf6c38f8a57d0c2ca0
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-03-25 13:57:43 -0300

    add pandas

--- database/db.py
+++ database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pandas as pd
 
 Base = declarative_base() # A base que serve para criação dos modelos do banco de dados
 
@@ -9,3 +10,6 @@ engine = create_engine('sqlite:///finance.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+'''df_user = pd.read_sql('SELECT * FROM user',engine)
+df_transac = pd.read_sql('SELECT * FROM transaction',engine)'''
