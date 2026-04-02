1d015b9fbd27260d7dce34fd2f08d983a043a83d
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-04-02 17:57:16 -0300

    modificações nas funções e importações de category

---
+++ routers/categoryRoutes.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter,status
+from database.db import session
+from database.models import Category
+from schemas.schemas import CategoryBase
+
+category_router = APIRouter(
+    prefix='/finance/category',
+    tags=['categories'] 
+          )
+
+@category_router.get('/')
+def request_return_categories():
+    categories = session.query(Category).all()
+    return [cat.dici() for cat in categories]
+
+@category_router.post('/')
+def request_register_category(request: CategoryBase):
+     new_category = Category(type=request.type,description=request.description)
+     session.add(request)
+     session.commit()
+
+@category_router.put('/{id}')
+def request_update_cateogry_infos(request: CategoryBase):
+     pass
