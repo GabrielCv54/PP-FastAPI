c43a6de8abf92f075a7d72d2d04823e403e9b8e1
Gabriel <gabriel.carvalhovieira123@gmail.com>
2026-04-06 08:21:19 -0300

    mudanças no accountRoutes

--- routers/accountRoutes.py
+++ routers/accountRoutes.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Request
-from schemas.schemas import AccountBase,AccountResponse
+from fastapi import APIRouter, Request,status
+from schemas.schemas import AccountBase,AccountResponse,NewAccountBase
 from database.models import Account
 from database.db import session
+from erros.errors import AccountNotFound
 
 account_router = APIRouter(
     prefix='/finance/accounts',
-    tags=['conta']
+    tags=['accounts']
 )
 
 @account_router.get("/")
@@ -14,9 +15,15 @@ async def request_return_accounts():
     return [accou.dici() for accou in accounts]
 
 @account_router.get("/{id}")
-async def request_returnId_accounts():
+async def request_returnId_accounts(request):
     pass
 
-@account_router.post('/')
-async def request_create_accounts():
-    pass
+@account_router.post('/',status_code=status.HTTP_201_CREATED)
+async def request_create_accounts(request: NewAccountBase):
+    try:
+        account = Account(type=request.type, balance=request.balance, status=request.status, agency_number=request.agency_number, client_id=request.client_id,bank=request.bank)
+        session.add(account)
+        session.commit()
+        return {'Sucesso':'A conta foi criada com sucesso!!'}
+    except:
+        raise {"Erro": AccountNotFound}
